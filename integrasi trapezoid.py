deebb56cf87a05684df4696000899ae42e8d1d39
Fikri Rida Pebriansyah <33893650+fikriultimate18@users.noreply.github.com>
2019-06-26 01:43:34 +0700

    Imprvmnt1

--- integrasi trapezoid.py
+++ integrasi trapezoid.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-print ("Aplikasi integrasi metode Simpson")
+print ("Aplikasi integrasi metode trapezoid")
 print ("app version 1.0 alpha\n")
 
 a = 1 #batas bawah
@@ -23,24 +23,14 @@ for i in list(range(n+1)):
 
 #y = -0,0081x3 + 0,1951x2 - 1,6411x + 14,355
 
-odd = [0 for it in list(range(n))]
-even = [0 for it in list(range(n))]
-
-sum_odd = 0
-sum_even = 0
-
-for i in list(range(1,n,2)):
-    sum_odd += y[i] 
-
-for i in list(range(2,n,2)):
-    sum_even += y[i] 
-
-print("\nn\tx\ty")
+sum_f = 0
+for it in list(range(1,n,1)):
+    sum_f += y[it]
 
 for o in list(range(n+1)):
     print(str(o) + "\t" + str(round(x[o],2)) + "\t" + str(round(y[o],2)))
 
-L = h*(y[0] + 4*sum_odd + 2*sum_even + y[n])/3
+L = (h/2) * (y[0] + 2*sum_f + y[n])
 
 print("\nHasil itegrasi\t:" + str(round(L,3)))
 
