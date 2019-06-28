a868e280ac5b0e617ebe3fad78b96e58fc82aad7
Fikri Rida Pebriansyah <33893650+fikriultimate18@users.noreply.github.com>
2019-06-28 17:24:09 +0700

    Imprvmnt

--- integrasi simpson.py
+++ integrasi simpson.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 print ("Aplikasi integrasi metode Simpson")
-print ("app version 1.0 alpha\n")
+print ("app version 1.0 beta\n")
 
 a = 1 #batas bawah
 b = 16 #batas atas
@@ -12,16 +12,17 @@ h = (b-a)/n #kepadatan
 print("batas bawah\t:" + str(a))
 print("batas atas\t:" + str(b))
 print("jml iterasi\t:" + str(n))
-print("besar h\t\t:" + str(h))
+print("besar h\t\t:" + str(round(h, 4)))
 
 x = [0 for it in list(range(n+1))]
 y = [0 for it in list(range(n+1))]
+
+#y = -0,0081x3 + 0,1951x2 - 1,6411x + 14,355
 
 for i in list(range(n+1)):
     x[i] = a+i*h
     y[i] = -0.0081*(pow(x[i],3)) + 0.1951*pow(x[i],2) - 1.6411*x[i] + 14.355
 
-#y = -0,0081x3 + 0,1951x2 - 1,6411x + 14,355
 
 odd = [0 for it in list(range(n))]
 even = [0 for it in list(range(n))]
@@ -35,14 +36,14 @@ for i in list(range(1,n,2)):
 for i in list(range(2,n,2)):
     sum_even += y[i] 
 
-print("\nn\tx\ty")
+print("\nn\tx\t\ty")
 
 for o in list(range(n+1)):
-    print(str(o) + "\t" + str(round(x[o],2)) + "\t" + str(round(y[o],2)))
+    print(str(o) + "\t" + str(round(x[o],4)) + "\t\t" + str(round(y[o],4)))
 
 L = h*(y[0] + 4*sum_odd + 2*sum_even + y[n])/3
 
-print("\nHasil itegrasi\t:" + str(round(L,3)))
+print("\nHasil itegrasi\t:" + str(round(L,4)))
 
 plt.plot(x,y, "go")
 plt.title("Grafik Arus terhadap Waktu")
--- integrasi trapezoid.py
+++ integrasi trapezoid.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 print ("Aplikasi integrasi metode trapezoid")
-print ("app version 1.0 alpha\n")
+print ("app version 1.0 beta\n")
 
 a = 1 #batas bawah
 b = 16 #batas atas
@@ -12,27 +12,29 @@ h = (b-a)/n #kepadatan
 print("batas bawah\t:" + str(a))
 print("batas atas\t:" + str(b))
 print("jml iterasi\t:" + str(n))
-print("besar h\t\t:" + str(h))
+print("besar h\t\t:" + str(round(h, 4)))
 
 x = [0 for it in list(range(n+1))]
 y = [0 for it in list(range(n+1))]
+
+#y = -0,0081x3 + 0,1951x2 - 1,6411x + 14,355
 
 for i in list(range(n+1)):
     x[i] = a+i*h
     y[i] = -0.0081*(pow(x[i],3)) + 0.1951*pow(x[i],2) - 1.6411*x[i] + 14.355
 
-#y = -0,0081x3 + 0,1951x2 - 1,6411x + 14,355
 
 sum_f = 0
 for it in list(range(1,n,1)):
     sum_f += y[it]
 
+print("\nn\tx\t\ty")
 for o in list(range(n+1)):
-    print(str(o) + "\t" + str(round(x[o],2)) + "\t" + str(round(y[o],2)))
+    print(str(o) + "\t" + str(round(x[o],4)) + "\t\t" + str(round(y[o],4)))
 
 L = (h/2) * (y[0] + 2*sum_f + y[n])
 
-print("\nHasil itegrasi\t:" + str(round(L,3)))
+print("\nHasil itegrasi\t:" + str(round(L,4)))
 
 plt.plot(x,y, "go")
 plt.title("Grafik Arus terhadap Waktu")
